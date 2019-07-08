a4c1b31d3c74c9ef906374d011e5242d23fdc172
Shelkopryad <welkopr9d@gmail.com>
2019-07-08 11:51:36 +0300

    Create routes.js

---
+++ frontend/src/routes.js
@@ -0,0 +1,6 @@
+export const urlGetCustomer = 'http://localhost:8080/getCustomer'
+export const urlAddCustomer = 'http://localhost:8080/addCustomer'
+export const urlGetTodo = 'http://localhost:8080/getTodo'
+export const urlGetTodos = 'http://localhost:8080/getTodos'
+export const urlCreateTodo = 'http://localhost:8080/createTodo'
+export const urlClearTodos = 'http://localhost:8080/clearTodos'
