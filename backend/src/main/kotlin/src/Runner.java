bff341bdd4514cd9f8d508404baff55006df0f54
Shelkopryad <welkopr9d@gmail.com>
2019-07-08 11:48:04 +0300

    Create Runner.java

---
+++ backend/src/main/kotlin/src/Runner.java
@@ -0,0 +1,25 @@
+package src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class Runner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
